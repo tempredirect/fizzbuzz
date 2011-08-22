41a5b97f3e019c9dacec29ef5fb7c296762ed072
Gareth Davis <gareth.davis@mac.com>
2011-08-22 19:22:03 +0100

    First commit

---
+++ src/main/java/com/logicalpractice/fizzbuzz/App.java
@@ -0,0 +1,13 @@
+package com.logicalpractice.fizzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
